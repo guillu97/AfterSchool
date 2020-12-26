766d883b1f7906d2134a536872e94477ef63ba47
Neyqoh <guillaume.farge@efrei.net>
2020-12-26 16:07:24 +0100

    first commit

---
+++ app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "afterschool-secret-key"
+  }; //TODO: secret key should be changed in prod
---
+++ server.js
@@ -0,0 +1,120 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+const db = require("./app/models");
+const Role = db.role;
+
+const dbConfig = require("./app/config/db.config");
+
+db.mongoose
+  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// index page
+app.get('/', function(req, res) {
+  var mascots = [
+      { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+      { name: 'Tux', organization: "Linux", birth_year: 1996},
+      { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+  ];
+  var tagline = "No programming concept is complete without a cute animal mascot.";
+
+  res.render('pages/index', {
+      mascots: mascots,
+      tagline: tagline
+  });
+});
+
+// about page
+app.get('/about', function(req, res) {
+  res.render('pages/about');
+});
+
+
+// for static files
+app.use('/static', express.static('public'));
+
+// simple route
+// app.get("/", (req, res) => {
+//   res.json({ message: "Welcome to bezkoder application." });
+// });
+
+// routes
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+
+
+
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'user' to roles collection");
+      });
+
+      new Role({
+        name: "moderator"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'moderator' to roles collection");
+      });
+
+      new Role({
+        name: "admin"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+}
